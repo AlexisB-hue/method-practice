5d4f491a03c7c6ebe2fc2a0a38eaafc503d56f67
Alexis Barrett <happynessb09@gmail.com>
2026-04-13 12:15:52 -0400

    greetings

---
+++ src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,31 @@
+
+
+public class sayHello {
+
+    public sayHello() {
+        System.out.println("Hello");
+    }
+}
+
+public class sayGoodbye {
+   public sayGoodbye(){
+       System.out.println("Goodbye");
+   }
+}
+
+public class sayGoodmorning {
+    public sayGoodmorning(){
+        System.out.println("Good morning");
+    }
+}
+
+
+public  void main(String[] args) {
+
+    sayHello hello = new sayHello();
+    sayGoodbye goodbye = new sayGoodbye();
+    sayGoodmorning goodmorning = new sayGoodmorning();
+    
+}
+
+
